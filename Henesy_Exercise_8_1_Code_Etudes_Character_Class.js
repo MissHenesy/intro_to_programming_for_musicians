708c06ac1ab3b9696b6669842022108ab077920e
Susan Henesy <shenesy@berklee.edu>
2019-03-04 23:38:21 -0500

    Adds code for character class

---
+++ Henesy_Exercise_8_1_Code_Etudes_Character_Class.js
@@ -0,0 +1,49 @@
+class Character
+{
+  constructor(name, age, fave_song)
+  {
+    this.name = name;
+    this.age = age;
+    this.fave_song = fave_song;
+  }
+
+  sayMyName()
+  {
+    return `My name is ${this.name}, and my favourite song is "${this.fave_song}."`;
+  }
+
+  compareAges(other_char)
+  {
+    if (this.age < other_char.age)
+    {
+      return `${this.name} is younger than ${other_char.name}.`;
+    } else if (this.age === other_char.age) {
+      return `${this.name} is the same age as ${other_char.name}.`;
+    } else {
+      return `${this.name} is older than ${other_char.name}.`;
+    }
+  }
+}
+
+
+let beatle1 = new Character("John", 23, "You Make Me Dizzy Miss Lizzie");
+let beatle2 = new Character("Paul", 21, "It's So Easy");
+let beatle3 = new Character("George", 20, "Everybody's Trying to be My Baby");
+let beatle4 = new Character("Ringo", 23, "South of the Border");
+let chars = [beatle1, beatle2, beatle3, beatle4];
+
+// Say a little something about each character.
+for (i of chars)
+{
+  console.log(i.sayMyName());
+}
+console.log("---------------------------------------------");
+
+// Compare ages of characters.
+for (i in chars)
+{
+  if (chars[parseInt(i)+1])
+  {
+    console.log(chars[i].compareAges(chars[parseInt(i)+1]));
+  }
+}
