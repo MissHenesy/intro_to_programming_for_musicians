165af3c807c3d064ad4f2bc91b9ab89a898cff68
Susan Henesy <shenesy@berklee.edu>
2019-03-05 21:05:07 -0500

    Tidies up code in Assignment 8

--- Henesy_Assignment_8_1_MySet_Revisited.js
+++ Henesy_Assignment_8_1_MySet_Revisited.js
@@ -35,7 +35,6 @@ class MySet
     // Using "Array.from" will clone our set,
     // so we won't have to worry about changes
     // to the array affecting our original set.
-    //console.log(this);
     return Array.from(this._dataset);
   }
 
@@ -58,57 +57,3 @@ class MySet
     return true;
   }
 }
-//----------------------------------------------------------------------------
-let ms1 = new MySet();
-ms1.add(4);
-ms1.add(4);
-ms1.add(5);
-ms1.add(6);
-console.log(ms1);
-console.log(ms1.toArray());
-// prints 4, 5, 6
-
-let ms2 = new MySet([1,2,1,3]);
-console.log(ms2);
-let ms2_arr = ms2.toArray();
-console.log(ms2_arr);
-
-ms2_arr.push(86, 87, 88);
-console.log("ms2 array: " + ms2_arr);
-console.log("ms2 dataset: ");
-console.log(ms2);
-
-let ms3 = ms2.clone();
-console.log("ms3 dataset: ");
-console.log(ms3);
-
-console.log("ms2 size is: " + ms2.size());
-console.log("ms3 size is: " + ms3.size());
-console.log(ms2.has(3));
-ms2.remove(3);
-ms2.remove(1);
-ms2.remove(2);
-ms2.add(4);
-ms2.add(5);
-ms2.add(6);
-console.log(ms2.has(3));
-console.log("Is ms2 = ms3?");
-console.log(ms2.is_equal(ms3));
-console.log("----------------------------------");
-ms1.for_each();
-console.log("----------------------------------");
-ms2.for_each();
-console.log("----------------------------------");
-ms3.for_each();
-console.log("----------------------------------");
-
-let ms4 = new MySet(["tea", "coffee", "milk"]);
-ms4.add("sugar").add("milk");
-ms4.add("cream");
-ms4.add(5);
-ms4.add("spoon");
-let ms4_arr = ms4.toArray();
-ms4_arr.push("saucer","scone","cucumber sandwich");
-console.log(ms4_arr);
-let foreach_vals = ms4.for_each();
-console.log(foreach_vals);
--- spec/Henesy_Assignment_8_1_MySet_Revisited_spec.js
+++ spec/Henesy_Assignment_8_1_MySet_Revisited_spec.js
@@ -5,21 +5,17 @@ describe(`Testing Assignment 8.1, 'MySet Revisited'.
   beforeEach(function() {
     test_set = new MySet();
   });
+  
   it ("should add an item", () => {
     test_set.add("tea");
     expect(test_set.size()).toBe(1);
-  });
-  it ("should receive duplicated items but only contain unique items", () => {
-    let compare_set = new MySet([1,2,3]);
-    test_set.add(1).add(1).add(1).add(2).add(2).add(3).add(3).add(3);
-    expect(test_set.is_equal(compare_set)).toBe(true);
   });
   it ("should remove an item", () => {
     test_set.add(1).add(2).add(2).add(3).add(3).add(3).add(4).add(5);
     test_set.remove(3);
     expect(test_set.has(3)).toBe(false);
   });
-  it ("should create a clone without impacting the original set", () => {
+  it ("should clone a set without affecting the original set", () => {
     test_set.add("raindrops on roses");
     test_set.add("whiskers on kittens");
     test_set.add("bright copper kettles");
@@ -32,6 +28,11 @@ describe(`Testing Assignment 8.1, 'MySet Revisited'.
     cloned_set.add("doorbells and sleighbells");
     cloned_set.add("schnitzel with noodles");
     expect(test_set.is_equal(cloned_set)).toBe(false);
+  });
+  it ("should receive duplicate items, but only contain unique items", () => {
+    let compare_set = new MySet([1,2,3]);
+    test_set.add(1).add(1).add(1).add(2).add(2).add(3).add(3).add(3);
+    expect(test_set.is_equal(compare_set)).toBe(true);
   });
   it ("should create an array of unique values from a set", () => {
     test_set.add(1).add(2).add(3).add(4).add(4).add(5);
